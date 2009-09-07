73ac0428c1dd9326c66d5d2279c8a566a654c8c4
Mauricio Fernandez <mfp@acm.org>
2009-09-07 12:17:41 +0200

    Schema tweaking (constraints and indexes for ACK-pending table).

--- schema.sql
+++ schema.sql
@@ -4,12 +4,14 @@ CREATE TABLE mq_server_msgs(
     msg_id VARCHAR(255) NOT NULL UNIQUE,
     priority INT NOT NULL,
     destination VARCHAR(255) NOT NULL,
-    "timestamp" TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     ack_timeout FLOAT NOT NULL,
     body TEXT NOT NULL
 );
 
-CREATE TABLE mq_server_ack_msgs(LIKE mq_server_msgs);
+CREATE TABLE mq_server_ack_msgs(LIKE mq_server_msgs INCLUDING DEFAULTS);
 
 CREATE INDEX mq_server_msgs_destination_priority_timestamp ON mq_server_msgs
        USING BTREE(priority, timestamp);
+
+CREATE UNIQUE INDEX mq_server_ack_msgs_msg_id ON mq_server_ack_msgs USING BTREE(msg_id);
