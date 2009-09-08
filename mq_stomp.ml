ec13d8b1287a29e9671cc12f4247e509c62a38b0
Mauricio Fernandez <mfp@acm.org>
2009-09-08 13:51:04 +0200

    Mq_stomp: ignore invalid header lines (not "k: v") when reading a frame.

--- mq_stomp.ml
+++ mq_stomp.ml
@@ -74,8 +74,9 @@ let read_stomp_headers ch =
     Lwt_io.read_line ch >>= function
         "" -> return acc
       | s ->
-          let k, v = String.split s ":" in
-            loop ((String.lowercase k, String.strip v) :: acc)
+          match (try Some (String.split s ":") with _ -> None) with
+              Some (k, v) -> loop ((String.lowercase k, String.strip v) :: acc)
+            | None -> (* just ignore this line *) loop acc
   in loop []
 
 let rec read_stomp_command ch =
